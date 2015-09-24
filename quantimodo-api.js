e8d320b89bddcee364d0c0128ce7d4da46002882
vitality <vitaly.kiktiov@gmail.com>
2015-09-24 16:19:55 +0300

    js warning in console when no access token
    https://github.com/Abolitionist-Project/QuantiModo-WordPress-Plugin/issues/87

--- quantimodo-api.js
+++ quantimodo-api.js
@@ -19,6 +19,7 @@ Quantimodo = function () {
             fetchAPI(0);
 
         } else {
+            console.warn('No access token. Now will try to authenticate and to get it');
             window.location.href = '?connect=quantimodo';
         }
 
@@ -87,6 +88,7 @@ Quantimodo = function () {
                 success: successHandler
             });
         } else {
+            console.warn('No access token. Now will try to authenticate and to get it');
             window.location.href = '?connect=quantimodo';
         }
     };
@@ -368,6 +370,7 @@ Quantimodo = function () {
                         }
                     });
                 } else {
+                    console.warn('No access token. Now will try to authenticate and to get it');
                     window.location.href = '?connect=quantimodo';
                 }
 
