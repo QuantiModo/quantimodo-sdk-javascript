418b82869d218393ca301434437d6ce85ccab31f
Mike P. Sinn <m@quantimodo.com>
2019-11-17 09:50:43 -0600

    tsconfig

--- test/api/UnitsApi.spec.js
+++ test/api/UnitsApi.spec.js
@@ -1,6 +1,7 @@
 /**
  * quantimodo
- * We make it easy to retrieve and analyze normalized user data from a wide array of devices and applications. Check out our [docs and sdk's](https://github.com/QuantiModo/docs) or [contact us](https://help.quantimo.do).
+ * We make it easy to retrieve and analyze normalized user data from a wide array of devices and applications. Check
+ * out our [docs and sdk's](https://github.com/QuantiModo/docs) or [contact us](https://help.quantimo.do).
  *
  * OpenAPI spec version: 5.8.112511
  *
@@ -13,63 +14,58 @@
  *
  */
 
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD.
-    define(['expect.js', '../../src/index'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // CommonJS-like environments that support module.exports, like Node.
-    factory(require('expect.js'), require('../../src/index'));
-  } else {
-    // Browser globals (root is window)
-    factory(root.expect, root.Quantimodo);
-  }
-}(this, function(expect, Quantimodo) {
-  'use strict';
-
-  var instance;
-
-  beforeEach(function() {
-    instance = new Quantimodo.UnitsApi();
-  });
-
-  var getProperty = function(object, getter, property) {
-    // Use getter method if present; otherwise, get the property directly.
-    if (typeof object[getter] === 'function')
-      return object[getter]();
-    else
-      return object[property];
-  }
-
-  var setProperty = function(object, setter, property, value) {
-    // Use setter method if present; otherwise, set the property directly.
-    if (typeof object[setter] === 'function')
-      object[setter](value);
-    else
-      object[property] = value;
-  }
-
-  describe('UnitsApi', function() {
-    describe('getUnitCategories', function() {
-      it('should call getUnitCategories successfully', function(done) {
-        //uncomment below and update the code to test getUnitCategories
-        //instance.getUnitCategories(function(error) {
-        //  if (error) throw error;
-        //expect().to.be();
-        //});
-        done();
-      });
+(function(root, factory){
+    if(typeof define === 'function' && define.amd){
+        // AMD.
+        define(['expect.js', '../../src/index'], factory);
+    }else if(typeof module === 'object' && module.exports){
+        // CommonJS-like environments that support module.exports, like Node.
+        factory(require('expect.js'), require('../../src/index'));
+    }else{
+        // Browser globals (root is window)
+        factory(root.expect, root.Quantimodo);
+    }
+}(this, function(expect, Quantimodo){
+    'use strict';
+    var instance;
+    beforeEach(function(){
+        instance = new Quantimodo.UnitsApi();
     });
-    describe('getUnits', function() {
-      it('should call getUnits successfully', function(done) {
-        //uncomment below and update the code to test getUnits
-        //instance.getUnits(function(error) {
-        //  if (error) throw error;
-        //expect().to.be();
-        //});
-        done();
-      });
+    var getProperty = function(object, getter, property){
+        // Use getter method if present; otherwise, get the property directly.
+        if(typeof object[getter] === 'function')
+            return object[getter]();
+        else
+            return object[property];
+    }
+    var setProperty = function(object, setter, property, value){
+        // Use setter method if present; otherwise, set the property directly.
+        if(typeof object[setter] === 'function')
+            object[setter](value);
+        else
+            object[property] = value;
+    }
+    describe('UnitsApi', function(){
+        describe('getUnitCategories', function(){
+            it('should call getUnitCategories successfully', function(done){
+                //uncomment below and update the code to test getUnitCategories
+                //instance.getUnitCategories(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+            });
+        });
+        describe('getUnits', function(){
+            it('should call getUnits successfully', function(done){
+                //uncomment below and update the code to test getUnits
+                instance.getUnits(function(error, data){
+                    if(error) throw error;
+                    expect(data).to.be.an('array');
+                    expect(data).to.have.length(62);
+                });
+                done();
+            });
+        });
     });
-  });
-
 }));
