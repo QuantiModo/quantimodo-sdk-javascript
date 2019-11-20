1fed66d8b7eb143e441b4f122c0980a4b7279c34
Mike P. Sinn <m@quantimodo.com>
2019-11-19 22:51:40 -0600

    source scripts/cypress.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,19 +3,4 @@ set +x && called=$_ && [[ ${called} != $0 ]]
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 CURRENT_FOLDER=`dirname ${SCRIPT_PATH}` && cd ${CURRENT_FOLDER} && cd .. && export REPO_DIR="$PWD" && set -x
 source scripts/output_commit_message_and_env.sh
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' nodejs|grep "install ok installed")
-echo Checking for nodejs: ${PKG_OK}
-if [[ "" == "$PKG_OK" ]]; then
-    echo "nodejs not found so installing..."
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-fi
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' xvfb|grep "install ok installed")
-echo Checking for xvfb: ${PKG_OK}
-if [[ "" == "$PKG_OK" ]]; then
-    echo "xvfb not found so installing..."
-    sudo apt-get install -y xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
-fi
-set -e
-npm install
-npm run ci
+source scripts/cypress.sh
--- scripts/cypress.sh
+++ scripts/cypress.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 called=$_ && [[ ${called} != $0 ]] && echo "${BASH_SOURCE[@]} is being sourced" || echo "${0} is being run"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_FOLDER=`dirname ${SCRIPT_PATH}` && cd ${SCRIPT_FOLDER} && cd .. && export REPO_DIR="$PWD"
@@ -8,6 +7,20 @@ rm cypress.env.json
 export RELEASE_STAGE="${RELEASE_STAGE:-staging}"
 cp cypress/config/cypress.${RELEASE_STAGE}.json cypress.json
 set +x
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' xvfb|grep "install ok installed")
+echo Checking for xvfb: ${PKG_OK}
+if [[ "" == "$PKG_OK" ]]; then
+    echo "xvfb not found so installing..."
+    sudo apt-get install -y xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+fi
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' nodejs|grep "install ok installed")
+echo Checking for nodejs: ${PKG_OK}
+if [[ "" == "$PKG_OK" ]]; then
+    echo "nodejs not found so installing..."
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
+set -e
 npm install
 echo "Saving host environment variables to host.env to access within docker"
 printenv > ${REPO_DIR}/.env
