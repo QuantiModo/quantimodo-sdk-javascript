5cbba8187ac7e03d20a32939e1ca52193af18d0f
Mike P. Sinn <m@quantimodo.com>
2019-11-19 19:57:38 -0600

    nodejs not found so installing...

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,5 +4,14 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SO
 CURRENT_FOLDER=`dirname ${SCRIPT_PATH}` && cd ${CURRENT_FOLDER} && cd .. && export REPO_DIR="$PWD" && set -x
 
 source scripts/output_commit_message_and_env.sh
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' nodejs|grep "install ok installed")
+echo Checking for somelib: $PKG_OK
+if [[ "" == "$PKG_OK" ]]; then
+    echo "nodejs not found so installing..."
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
+
 npm install
 npm run ci
