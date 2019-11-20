f92fdf4aff9881cc6d82a4ed4309c3e7b6e4a2ad
Mike P. Sinn <m@quantimodo.com>
2019-11-19 18:57:07 -0600

    npm install

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,4 +3,5 @@ set -e && set +x && called=$_ && [[ ${called} != $0 ]]
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 CURRENT_FOLDER=`dirname ${SCRIPT_PATH}` && cd ${CURRENT_FOLDER} && cd .. && export REPO_DIR="$PWD" && set -x
 
-source ${REPO_DIR}/scripts/cypress.sh
+npm install
+npm run ci
