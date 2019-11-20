25caeab91de18a662b50e49776c7587f52cae64d
Mike P. Sinn <m@quantimodo.com>
2019-11-19 21:43:11 -0600

    sudo apt-get install -y xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,6 +10,12 @@ if [[ "" == "$PKG_OK" ]]; then
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get install -y nodejs
 fi
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' xvfb|grep "install ok installed")
+echo Checking for xvfb: ${PKG_OK}
+if [[ "" == "$PKG_OK" ]]; then
+    echo "xvfb not found so installing..."
+    sudo apt-get install -y xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2
+fi
 set -e
 npm install
 npm run ci
