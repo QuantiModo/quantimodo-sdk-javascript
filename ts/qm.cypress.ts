6f89f5e02e558a110029f63512385dfaae7d0c4c
Mike P. Sinn <m@quantimodo.com>
2019-11-24 12:47:00 -0600

    import * as dotenv from "dotenv";

--- ts/qm.cypress.ts
+++ ts/qm.cypress.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 dotenv.config(); // https://github.com/motdotla/dotenv#what-happens-to-environment-variables-that-were-already-set
 import * as qmTests from "./qm.tests";
 qmTests.runLastFailedCypressTest(function(err: any): void {
