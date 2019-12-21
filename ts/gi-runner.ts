04b9518569593e479d1cd52c9b85d7589b3c4910
Mike P. Sinn <m@quantimodo.com>
2019-12-21 11:52:11 -0600

    // process.env.RELEASE_STAGE = "staging"

--- ts/gi-runner.ts
+++ ts/gi-runner.ts
@@ -2,7 +2,7 @@
 // npm install typescript ts-node
 // npx ts-node ts/gi-run.ts
 import * as gi from "./gi-functions"
-process.env.RELEASE_STAGE = "staging"
+// process.env.RELEASE_STAGE = "staging"
 gi.runEverything(function() {
     process.exit(0)
 })
