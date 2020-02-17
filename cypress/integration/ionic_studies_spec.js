f795adc2b5f9bd9d6e6f6d90fdb9ff3b62d20f8e
Mike P. Sinn <m@quantimodo.com>
2020-02-17 12:58:15 -0600

    cy.contains('Join This Study')

--- cypress/integration/ionic_studies_spec.js
+++ cypress/integration/ionic_studies_spec.js
@@ -78,7 +78,7 @@ describe('Studies', function () {
   })
   it('Joins study from static v2/study page', function () {
     cy.visitApi(`/api/v2/study?logLevel=info&effectVariableName=Overall%20Mood&causeVariableName=Flaxseed%20Oil`)
-    cy.get('#joinStudyButton')
+    cy.contains('Join This Study')
         .invoke('removeAttr', 'target') // Cypress can't follow to new tab
         .click({ force: true })
     cy.wait(5000)
